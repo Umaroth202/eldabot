61164b6520dec2f02800cd97cce7f635d740c578
Umaroth202 <77385765+Umaroth202@users.noreply.github.com>
2021-02-07 19:57:43 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,164 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+import asyncio
+
+bot = commands.Bot(command_prefix = "!", description = "Bot du sevreur discord Eldar.")
+musics = {}
+ytdl = youtube_dl.YoutubeDL()
+
+@bot.event
+async def on_ready():
+	print("Ready !")
+	
+@bot.event
+async def on_ready():
+	await bot.change_presence(activity=discord.Game(name="Eldar | https://discord.gg/ADJmhADX96"))
+
+@bot.command()
+async def hello(ctx):
+	hello = "Hello !"
+	await ctx.send(hello)
+
+@bot.command()
+async def serverinfo(ctx):
+	server = ctx.guild
+	numberOfTextChannels = len(server.text_channels)
+	numberOfVoiceChannels = len(server.voice_channels)
+	serverDescription = server.description
+	numberOfPerson = server.member_count
+	serverName = server.name
+	message = f"Le serveur **{serverName}** contient *{numberOfPerson}* personnes ! \nLa description du serveur est {serverDescription}. \nCe serveur possède {numberOfTextChannels} salons écrit et {numberOfVoiceChannels} salon vocal/vocaux."
+	await ctx.send(message)
+	
+@bot.command()
+async def botinfo(ctx):
+	botinfo = f"**__Je suis le bot du serveur Eldar !__** \n \n**__Présentation :__** \n**Pseudo** : EldaBot \n **Discriminateur** : #8705 \n**Pseudo et Tag** : EldaBot#8705 \n \n**__Informations Diverses :__** \n**Dévelloppé par** : Umaroth#5340 \n **Lien du Serveur __Eldar__** : https://discord.gg/ADJmhADX96"
+	await ctx.send(botinfo)
+	
+@bot.command()
+async def ban(ctx, user : discord.User, *reason):
+	reason = " ".join(reason)
+	await ctx.guild.ban(user, reason = reason)
+	await ctx.send(f"{user} à été ban pour la raison suivante : {reason}.")
+
+@bot.command()
+async def unban(ctx, user, *reason):
+	reason = " ".join(reason)
+	userName, userId = user.split("#")
+	bannedUsers = await ctx.guild.bans()
+	for i in bannedUsers:
+		if i.user.name == userName and i.user.discriminator == userId:
+			await ctx.guild.unban(i.user, reason = reason)
+			await ctx.send(f"{user} à été unban.")
+			return
+	#Ici on sait que lutilisateur n'a pas été trouvé.'
+	await ctx.send(f"L'utilisateur {user} n'est pas dans la liste des bans")
+
+@bot.command()
+async def kick(ctx, user : discord.User, *reason):
+	reason = " ".join(reason)
+	await ctx.guild.kick(user, reason = reason)
+	await ctx.send(f"{user} à été kick.")
+
+@bot.command()
+async def clear(ctx, nombre : int):
+	messages = await ctx.channel.history(limit = nombre + 1).flatten()
+	for message in messages:
+		await message.delete()
+		
+async def createMutedRole(ctx):
+    mutedRole = await ctx.guild.create_role(name = "Muted",
+                                            permissions = discord.Permissions(
+                                                send_messages = False,
+                                                speak = False),
+                                            reason = "Creation du role Muted pour mute des gens.")
+    for channel in ctx.guild.channels:
+        await channel.set_permissions(mutedRole, send_messages = False, speak = False)
+    return mutedRole
+
+async def getMutedRole(ctx):
+    roles = ctx.guild.roles
+    for role in roles:
+        if role.name == "Muted":
+            return role
+    
+    return await createMutedRole(ctx)
+
+@bot.command()
+async def mute(ctx, member : discord.Member, *, reason = "Aucune raison n'a été renseigné"):
+    mutedRole = await getMutedRole(ctx)
+    await member.add_roles(mutedRole, reason = reason)
+    await ctx.send(f"{member.mention} a été mute !")
+
+@bot.command()
+async def unmute(ctx, member : discord.Member, *, reason = "Aucune raison n'a été renseigné"):
+    mutedRole = await getMutedRole(ctx)
+    await member.remove_roles(mutedRole, reason = reason)
+    await ctx.send(f"{member.mention} a été unmute !")
+    
+class Video:
+    def __init__(self, link):
+        video = ytdl.extract_info(link, download=False)
+        video_format = video["formats"][0]
+        self.url = video["webpage_url"]
+        self.stream_url = video_format["url"]
+
+@bot.command()
+async def leave(ctx):
+    client = ctx.guild.voice_client
+    await client.disconnect()
+    musics[ctx.guild] = []
+
+@bot.command()
+async def resume(ctx):
+    client = ctx.guild.voice_client
+    if client.is_paused():
+        client.resume()
+
+
+@bot.command()
+async def pause(ctx):
+    client = ctx.guild.voice_client
+    if not client.is_paused():
+        client.pause()
+
+
+@bot.command()
+async def skip(ctx):
+    client = ctx.guild.voice_client
+    client.stop()
+
+
+def play_song(client, queue, song):
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url
+        , before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
+
+    def next(_):
+        if len(queue) > 0:
+            new_song = queue[0]
+            del queue[0]
+            play_song(client, queue, new_song)
+        else:
+            asyncio.run_coroutine_threadsafe(client.disconnect(), bot.loop)
+
+    client.play(source, after=next)
+
+
+@bot.command()
+async def play(ctx, url):
+    print("play")
+    client = ctx.guild.voice_client
+
+    if client and client.channel:
+        video = Video(url)
+        musics[ctx.guild].append(video)
+    else:
+        channel = ctx.author.voice.channel
+        video = Video(url)
+        musics[ctx.guild] = []
+        client = await channel.connect()
+        await ctx.send(f"Je lance : {video.url}")
+        play_song(client, musics[ctx.guild], video)
+
+bot.run("VOTRE_TOKEN")
